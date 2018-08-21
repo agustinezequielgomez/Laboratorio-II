facdc3446a966a369a744a7bc04880e310a9bc2b
agustinezequielgomez <36157004+agustinezequielgomez@users.noreply.github.com>
2018-08-21 12:15:53 -0300

    Add files via upload

---
+++ Gomez.Agustin.2A/Ejercicio00/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio00
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre;
+            int edad;
+            Console.Write("Ingrese su nombre: ");
+            nombre=Console.ReadLine();
+            Console.WriteLine("Su nombre es {0}", nombre);
+            Console.ReadLine();
+            Console.Write("Ingrese su edad: ");
+            edad = int.Parse(Console.ReadLine());
+            Console.WriteLine("Su nombre es {0} y su edad es {1}", nombre, edad);
+            Console.ReadLine();  
+        }
+    }
+}
