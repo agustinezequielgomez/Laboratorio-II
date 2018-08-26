d5a021309bc903c9f3c8bd2581fa7fb8f079b03e
agustinezequielgomez <agusgomez99@gmail.com>
2018-08-26 18:31:28 -0300

    Ejercicio 11

--- Gomez.Agustin.2A/Ejercicios Guia/Ejercicio11/Program.cs
+++ Gomez.Agustin.2A/Ejercicios Guia/Ejercicio11/Program.cs
@@ -11,7 +11,43 @@ namespace Ejercicio11
         static void Main(string[] args)
         {
             Console.Title = "Ejercicio Nro 11";
+            int numeros;
+            int i;
+            float total = 0;
+            int minimo = 0;
+            int maximo = 0;
+            bool flag = true;
 
+            for (i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Ingresa un numero ");
+                numeros = int.Parse(Console.ReadLine());
+                if (Validacion.Validar(numeros, -100, 100))
+                {
+                    if (flag == true)
+                    {
+                        flag = false;
+                        maximo = numeros;
+                        minimo = numeros;
+                    }
+
+                    if (numeros > maximo)
+                    {
+                        maximo = numeros;
+                    }
+                    else if (numeros < minimo)
+                    {
+                        minimo = numeros;
+                    }
+                    total += numeros;
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine("El numero maximo es {0}", maximo);
+            Console.WriteLine("El numero minimo es {0}", minimo);
+            Console.WriteLine("El promedio total es {0:#.00}", total / 10);
+            Console.ReadLine();
         }
     }
 }
+
