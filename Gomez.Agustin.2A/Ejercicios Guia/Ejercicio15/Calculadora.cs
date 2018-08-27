1a90b398aa19c6a59e2872e7a305bbb639e57d83
agustinezequielgomez <agusgomez99@gmail.com>
2018-08-27 20:33:06 -0300

    Ejercicios terminados

---
+++ Gomez.Agustin.2A/Ejercicios Guia/Ejercicio15/Calculadora.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio15
+{
+    class Calculadora
+    {
+        public static float Calcular(float numeroUno, float numeroDos, char signo)
+        {
+            float operacion = 0;
+            switch(signo)
+            {
+                case '+':
+                    operacion = numeroUno + numeroDos;
+                    break;
+                case '-':
+                    operacion = numeroUno - numeroDos;
+                    break;
+                case '*':
+                    operacion = numeroUno * numeroDos;
+                    break;
+                    case '/':
+                    if(Calculadora.Validar(numeroDos))
+                    {
+                        operacion = numeroUno / numeroDos;
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("No es posible dividir ya que el 2do numero es 0");
+                        Console.ResetColor();
+                    }
+                    break;
+                default:
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.Write("La operacion matematica ingresada no es valida!");
+                    Console.ReadLine();
+                    Console.ResetColor();
+                    break;
+            }
+            return operacion;
+        }
+
+        private static bool Validar(float numeroDos)
+        {
+            if(numeroDos!=0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static void Mostrar(float resultado)
+        {
+            Console.WriteLine("El resultado de la operacion es {0}", resultado);
+        }
+    }
+}
---
+++ Gomez.Agustin.2A/Ejercicios Guia/Ejercicio15/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio15
+{
+    class Ejercicio_15
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Ejercicio 15";
+            float numeroUno;
+            float numeroDos;
+            char operacion;
+            char continuar;
+            do
+            {
+                Console.Write("Ingresa el primer numero ");
+                numeroUno = float.Parse(Console.ReadLine());
+                Console.Write("Ingresa el segundo numero ");
+                numeroDos = float.Parse(Console.ReadLine());
+                Console.Write("Ingresa la operacion que quieras realizar (+,-,/,*) ");
+                operacion = char.Parse(Console.ReadLine());
+                Calculadora.Mostrar(Calculadora.Calcular(numeroUno, numeroDos, operacion));
+                Console.Write("Desea continuar con el programa (S/N) ");
+                continuar = char.Parse(Console.ReadLine());
+                Console.Clear();
+            } while (continuar == 's' || continuar == 'S');
+        }
+    }
+}
