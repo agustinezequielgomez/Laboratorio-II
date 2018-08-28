d87cef8813b7c37f7bf605bdfc758ad3546346aa
alumno <alumno>
2018-08-28 09:15:30 -0300

    Clase 03

---
+++ Gomez.Agustin.2A/EjercicioClase03/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioClase03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sello.mensaje = "1";
+            Console.Write(Sello.Imprimir());
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Gomez.Agustin.2A/EjercicioClase03/Sello.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioClase03
+{
+    class Sello
+    {
+        public static string mensaje;
+        public static ConsoleColor color;
+
+        public static string Imprimir()
+        {
+            return Sello.ArmarFormatoMensaje();
+        }
+
+        public static void Borrar()
+        {
+            Sello.mensaje = "";
+        }
+
+        public static void ImprimirEncolor()
+        {
+            Console.BackgroundColor = Sello.color;
+            Console.WriteLine(Sello.Imprimir()); //Encapsulamiento
+        }
+
+        private static string ArmarFormatoMensaje()
+        {
+            string mensaje = Sello.mensaje;
+            string cadenaReturn="";
+            string asteriscos = "";
+            string middleLine = "";
+            int largo = mensaje.Length;
+            int i;
+            for (i=0;i<largo+2;i++)
+            {
+                asteriscos += "*";
+            }
+            middleLine +="\n*";
+            middleLine += mensaje;
+            middleLine += "*\n";
+            cadenaReturn += asteriscos;
+            cadenaReturn += middleLine;
+            cadenaReturn += asteriscos;
+            return cadenaReturn;
+        }
+    }
+}
