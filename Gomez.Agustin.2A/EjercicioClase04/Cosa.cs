f3656ac9419ed2d8434e6da9a8b2079a60b37cba
alumno <alumno>
2018-08-29 10:46:00 -0300

    Clase 4. Sobrecarga de constructores

---
+++ Gomez.Agustin.2A/EjercicioClase04/Cosa.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioClase04
+{
+    class Cosa
+    {
+        public string cadena;
+        public double numero;
+        public DateTime fecha;
+
+        public static string Mostrar(Cosa objeto)
+        {
+            return objeto.Mostrar();
+        }
+
+        private string Mostrar()
+        {
+            string retorno = "";
+            retorno += this.cadena + "  " +this.numero.ToString() + "  " + this.fecha.ToString();
+            return retorno;
+        }
+
+        public Cosa()
+        {
+            this.cadena = "Sin valor";
+            this.numero = 1.9;
+            this.fecha = DateTime.Now;
+        }
+
+        public Cosa(string c)
+        {
+            this.cadena = c;
+            this.numero = 1.9;
+            this.fecha = DateTime.Now;
+        }
+    }
+}
---
+++ Gomez.Agustin.2A/EjercicioClase04/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioClase04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cosa objeto1 = new Cosa();
+            Console.WriteLine(Cosa.Mostrar(objeto1));
+
+            Cosa objeto2 = new Cosa("Jose");
+            Console.WriteLine(Cosa.Mostrar(objeto2));
+
+            Cosa objeto3 = new Cosa();
+            objeto3.cadena = "HOla";
+            objeto3.numero = 458;
+            objeto3.fecha = DateTime.Now;
+            Console.Write(Cosa.Mostrar(objeto3));
+            Console.ReadLine();
+        }
+    }
+}
