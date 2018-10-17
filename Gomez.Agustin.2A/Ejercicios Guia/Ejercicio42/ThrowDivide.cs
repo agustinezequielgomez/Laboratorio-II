8665b762150229f8e8ac76b3d2c66f3007f8905e
alumno <alumno>
2018-10-17 12:11:33 -0300

    Terminar 42

---
+++ Gomez.Agustin.2A/Ejercicios Guia/Ejercicio42/ThrowDivide.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio42
+{
+    class ThrowDivide
+    {
+        public static void Metodo()
+        {
+            throw new DivideByZeroException();
+        }
+    }
+}
---
+++ Gomez.Agustin.2A/Ejercicios Guia/Ejercicio42/UnaExcepcion.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio42
+{
+    class UnaExcepcion : Exception
+    {
+        public UnaExcepcion()
+        {
+            try
+            {
+                new ExcepctionA();
+            }
+            catch(Exception e)
+            {
+                throw e;
+            }
+        }
+    }
+}
