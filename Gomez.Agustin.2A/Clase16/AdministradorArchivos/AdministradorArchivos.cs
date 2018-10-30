c9787eba76ce3f3383ba51d1c2126c52c3f341d0
alumno <alumno>
2018-10-30 10:13:09 -0300

    Clase16

---
+++ Gomez.Agustin.2A/Clase16/AdministradorArchivos/AdministradorArchivos.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace EjercicioClase16
+{
+    static class AdministradorArchivos
+    {
+        public static bool Escribir(string path, string texto)
+        {
+            bool retorno = false;
+            try
+            {
+                using (StreamWriter stream = new StreamWriter(path, false))
+                {
+                    stream.Write(texto);
+                    retorno = true;
+                }
+            }
+            catch(Exception e)
+            {
+
+            }
+            return retorno;
+        }
+
+        public static bool Leer(string path, out string texto)
+        {
+            bool retorno = false;
+            texto = "";
+            try
+            {
+                using (StreamReader stream = new StreamReader(path))
+                {
+                    texto = stream.ReadToEnd();
+                    retorno = true;
+                }
+            }
+            catch(Exception e)
+            {
+            }
+            return retorno;
+        }
+    }
+}
---
+++ Gomez.Agustin.2A/Clase16/AdministradorArchivos/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioClase16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path1 = (Environment.GetFolderPath(Environment.SpecialFolder.Desktop)+@"\Archivo.txt");
+            string path2 = (Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)+@"\Archivo.txt");
+            string path3 = (Environment.GetFolderPath(Environment.SpecialFolder.MyPictures) + @"\Archivo.txt");
+            string pathBase = (AppDomain.CurrentDomain.BaseDirectory) + @"\Archivo.txt";
+            string texto;
+            AdministradorArchivos.Escribir(path1, "\nEscritorio");
+            if(AdministradorArchivos.Leer(path1, out texto))
+            {
+                Console.WriteLine(texto);
+            }
+            AdministradorArchivos.Escribir(path2, "\nDocumentos");
+            if (AdministradorArchivos.Leer(path2, out texto))
+            {
+                Console.WriteLine(texto);
+            }
+            AdministradorArchivos.Escribir(path3, "\nImagenes");
+            if (AdministradorArchivos.Leer(path3, out texto))
+            {
+                Console.WriteLine(texto);
+            }
+            AdministradorArchivos.Escribir(pathBase, "\nBase");
+            if (AdministradorArchivos.Leer(pathBase, out texto))
+            {
+                Console.WriteLine(texto);
+            }
+            Console.ReadKey();
+        }
+    }
+}
