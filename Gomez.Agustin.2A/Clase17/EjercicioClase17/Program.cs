09967a4e6e53f1c6f1d0f2cd504aa8192c1edbcb
alumno <alumno>
2018-11-06 12:13:26 -0300

    Clase 17

---
+++ Gomez.Agustin.2A/Clase17/EjercicioClase17/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace EjercicioClase17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Leo de SQL
+            List<Televisor> listaTelevisores = new List<Televisor>();
+            SqlConnection conexion = new SqlConnection(Properties.Settings.Default.Conexion); //Establezco una conexion al SQL Server
+            SqlCommand comando = new SqlCommand(); //Establezco un comando a ejecutar sobre la base de datos
+            comando.CommandType = CommandType.Text; //Establezco el tipo de comando a realizar
+            comando.CommandText = "SELECT * FROM Televisores"; //Establezco el comando en si
+            comando.Connection = conexion; //Le asigno al comando que va a realizarse una conexion al SQL Server
+            conexion.Open(); //Abro la Conexion
+            SqlDataReader lector = comando.ExecuteReader(); //Objeto que lee linea por linea la base de datos
+            while(lector.Read()) //Leo una linea y se le asigna al objeto lector
+            {
+                //Televisor televisor = new Televisor((int)lector["codigo"], (string)lector["marca"], (float)lector["precio"], (int)lector["pulgadas"], (string)lector["pais"]);
+                Televisor televisor = new Televisor(lector.GetInt32(0), lector.GetString(1), lector.GetDouble(2), lector.GetInt32(3), lector.GetString(4)); //Leo la base de datos
+                listaTelevisores.Add(televisor);
+                Console.WriteLine("{0}-{1}-{2}-{3}-{4}",lector["codigo"], lector["marca"], lector["precio"], lector["pulgadas"], lector["pais"]);
+            }
+            conexion.Close(); //Cierro la conexion al server
+            #endregion
+
+            #region XML
+            //Serializo la lista de teles en un XML.
+            XmlSerializer serializer = new XmlSerializer(typeof(List<Televisor>));
+            XmlTextWriter writer = new XmlTextWriter("Televisores.xml", Encoding.UTF8);
+            XmlTextReader reader = new XmlTextReader("Televisores.xml");
+
+            serializer.Serialize(writer, listaTelevisores);
+            writer.Close();
+            List<Televisor> listaDeserializada = (List<Televisor>)serializer.Deserialize(reader);
+            reader.Close();
+            #endregion
+
+            #region DataTable
+            conexion.Open();
+            lector = comando.ExecuteReader();
+            DataTable dataTable = new DataTable("Televisores"); //Creo una nueva datatable
+            dataTable.Load(lector); //Cargo los datos de la base de datos en el DataTable. Recibe un ejercicio SqlDataReader.
+            conexion.Close();
+
+            dataTable.WriteXmlSchema("Televisores_esquema.xml"); //Serializo la estructura del datatable en un xml.
+            dataTable.WriteXml("Televisores_dt.xml"); //Serializo los datos de un xml en el datatable
+
+            DataTable dataTableRead = new DataTable();
+            dataTableRead.ReadXmlSchema("Televisores_esquema.xml"); 
+            dataTableRead.ReadXml("Televisores_dt.xml");
+
+            //Agrego tele a la DataTable
+            Televisor televisorAgregar = new Televisor(1245, "Samsung", 15000, 32, "Japon");
+            Console.WriteLine(televisorAgregar.Insertar());
+            #endregion
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Gomez.Agustin.2A/Clase17/EjercicioClase17/Televisor.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace EjercicioClase17
+{
+    public class Televisor
+    {
+        #region Atributos
+        public int id;
+        public string marca;
+        public double precio;
+        public int pulgadas;
+        public string pais;
+        #endregion
+
+        #region Constructor
+        public Televisor()
+        {
+
+        }
+
+        public Televisor(int id, string marca, double precio, int pulgadas, string pais)
+        {
+            this.id = id;
+            this.marca = marca;
+            this.precio = precio;
+            this.pulgadas = pulgadas;
+            this.pais = pais;
+        }
+        #endregion
+
+        #region Metodos
+        public bool Insertar()
+        {
+            SqlConnection conexion = new SqlConnection(Properties.Settings.Default.Conexion);
+            SqlCommand command = new SqlCommand();
+            command.CommandType = System.Data.CommandType.Text;
+            command.CommandText = String.Format("INSERT INTO Televisores values({0},'{1}',{2},{3},'{4}')", this.id, this.marca, this.precio, this.pulgadas, this.pais);
+            command.Connection = conexion;
+            //string cadena = "INSERT INTO Televisores values("+this.id+",'"+this.marca+"',"+this.precio+","+this.pulgadas+",'"+this.pais+"')";
+
+            //command.CommandText = cadena;
+            try
+            {
+                conexion.Open();
+                command.ExecuteNonQuery(); //Ejecuto consultas que no retornan resultado
+                conexion.Close();
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+            return true;
+        }
+        #endregion
+    }
+}
